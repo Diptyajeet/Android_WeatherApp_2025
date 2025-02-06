4025135c64eebab78428ebf7a49d50210260b10d
Diptyajeet <diptyajeetkar@gmail.com>
2025-02-06 21:09:37 +0530

    Update greatestnumber.java

--- greatestnumber.java
+++ greatestnumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class greatestnumber {
+public class GreatestNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
